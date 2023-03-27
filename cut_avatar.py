9c81cc5db13962c60c2299482f475b826b267f0f
BAIBASH1 <gazinyr2014@mail.ru>
2023-03-28 01:46:34 +0300

    Initial commit

---
+++ cut_avatar.py
@@ -0,0 +1,29 @@
+from PIL import Image
+
+
+def crop_middle(image):
+    middle_crop_image = \
+        image.crop((int(count_pixel / 2), 0, int(image.width - count_pixel / 2), image.height))
+    return middle_crop_image
+
+
+def crop_left(image):
+    left_crop_image = image.crop((count_pixel, 0, image.width, image.height))
+    joined_image = Image.blend(left_crop_image, crop_middle(image), 0.5)
+    return joined_image
+
+
+def crop_right(image):
+    right_crop_image = image.crop((0, 0, image.width - count_pixel, image.height))
+    joined_image = Image.blend(right_crop_image, crop_middle(image), 0.5)
+    return joined_image
+
+
+import_image = Image.open('image.jpg')
+(red, green, blue) = import_image.split()
+count_pixel = 100
+
+new_avatar = Image.merge("RGB", (crop_left(red), crop_middle(green), crop_right(blue)))
+new_avatar.save('full_new_avatar.jpg')
+new_avatar.thumbnail((80, 80))
+new_avatar.save('80x80_new_avatar.jpg')
